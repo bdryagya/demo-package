a32e968c59c9e0cc212817f725dfed3fdb231a52
Yagya Chaudhary <mail@bdryagya.com.np>
2020-10-10 12:39:08 +0545

    Init

---
+++ src/Helper.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * <description>
+ *
+ * PHP version <version>
+ *
+ * @category <category>
+ * @package  <package>
+ * @author   "Yagya Chaudhary" <mail@bdryagya.com.np>
+ * @license  http://bdryagya.com.np/license.txt MIT
+ * @link     http://pages.yagya.pro.np
+ */
+
+namespace \Bdryagya\DemoLibrary;
+
+class Helper {
+  public function __construct() {
+
+  }
+
+  public function run() {
+    return "I'm Running";
+  }
+}
